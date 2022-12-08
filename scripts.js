12305bfa2e5f307bab1da606859a0b97ad489f74
TempestPoet <etienne.schaumburger@gmail.com>
2022-12-08 21:22:17 +0100

    crossout works

--- scripts.js
+++ scripts.js
@@ -6,7 +6,6 @@ const orderedWordList = document.getElementById("ordered-List");
 //var rndWord = "";
 var wordArray = [];
 
-
 // ****************************A D D - N E W - W O R D - T O - L I S T**********************************************************
 /***event listener for word entry on mouse click ****/
 wordSubmitButton.addEventListener("click", () => {
@@ -25,15 +24,19 @@ wordInputField.addEventListener("keydown", function (e) {
   /****checks, if word is a valid entry, blocks everything except small/capital letters  *****/
 });
 function validate(e) {
-  if (wordInputField.value.length > 2 && wordInputField.value.length < 11 &&  /[^a-zA-Z]+/g.test(wordInputField.value) === false) {  
+  if (
+    wordInputField.value.length > 2 &&
+    wordInputField.value.length < 11 &&
+    /[^a-zA-Z]+/g.test(wordInputField.value) === false
+  ) {
     let newAddList = document.createElement("li");
     newAddList.classList.add("listedItems");
-    newAddList.setAttribute("id", "newWordEntry");
+    newAddList.setAttribute("id", wordInputField.value.toLowerCase());
     newAddList.textContent = wordInputField.value.toLowerCase();
     orderedWordList.appendChild(newAddList);
     wordArray.push(newAddList.textContent);
     console.log(wordArray);
-  //  wordInputField.value = "";
+    //  wordInputField.value = "";
   } else {
     wordInputField.value = wordInputField.value.match(new RegExp(/[a-zA-Z]+/));
   }
@@ -44,7 +47,7 @@ var riddleArray = [
   ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"],
   ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19"],
   ["20", "21", "22", "23", "24", "25", "26", "27", "28", "29"],
-  ["30", "31", "32", "33", "34", "35", "36", "37", "38", "39"], 
+  ["30", "31", "32", "33", "34", "35", "36", "37", "38", "39"],
   ["40", "41", "42", "43", "44", "45", "46", "47", "48", "49"],
   ["50", "51", "52", "53", "54", "55", "56", "57", "58", "59"],
   ["60", "61", "62", "63", "64", "65", "66", "67", "68", "69"],
@@ -70,7 +73,7 @@ function addLetters() {
       document.getElementById("" + i + j).innerHTML = "0"; //newRandomLetter();
     })
   );
- // document.getElementById("cell75").innerHTML = "E"
+  // document.getElementById("cell75").innerHTML = "E"
 }
 //*************************************C L E A R - L I S T******************F U N C T I O N************* */
 function deleteList() {
@@ -87,7 +90,6 @@ function clearRiddle() {
   riddleArray.forEach((row, i) =>
     row.forEach((item, j) => {
       document.getElementById("" + i + j).innerHTML = "";
-      
     })
   );
   deleteList();
@@ -111,27 +113,26 @@ function testButton2() {
   var col = Math.floor(Math.random() * 10);
   var cell = document.getElementById("" + row + col);
   var switchRandomizer = Math.floor(Math.random() * 8) + 1;
-  
-  
+
   if (switchRandomizer > 4) {
     rndWord = reverseString(rndWord);
-  } 
+  }
 
   switch (switchRandomizer) {
     // ----- R O W S ------
     case 1:
       if (
-        (row + rndWord.length <= riddleArray.length) && 
-        (testWord(cell, row, col, switchRandomizer) === true)
+        row + rndWord.length <= riddleArray.length &&
+        testWord(cell, row, col, switchRandomizer) === true
       ) {
         for (let k = 0; k < rndWord.length; k++) {
           const cell = document.getElementById("" + row++ + col);
           cell.innerHTML = "";
           cell.innerHTML += rndWord[0 + k];
-          numberCompareArray.push(cell.id)
+          numberCompareArray.push(cell.id);
         }
       } else {
-       testButton2();
+        testButton2();
       }
       break;
     // ----- C O L U M N S ------
@@ -144,14 +145,14 @@ function testButton2() {
           const cell = document.getElementById("" + row + col++);
           cell.innerHTML = "";
           cell.innerHTML += rndWord[0 + k];
-          numberCompareArray.push(cell.id)
+          numberCompareArray.push(cell.id);
         }
       } else {
         testButton2();
       }
       break;
-   
-      // ----- D I A G O N A L - TopLeft to BottomRight------
+
+    // ----- D I A G O N A L - TopLeft to BottomRight------
     case 3:
       if (
         row + rndWord.length <= riddleArray.length &&
@@ -162,7 +163,7 @@ function testButton2() {
           const cell = document.getElementById("" + row++ + col++);
           cell.innerHTML = "";
           cell.innerHTML += rndWord[0 + k];
-          numberCompareArray.push(cell.id)
+          numberCompareArray.push(cell.id);
         }
       } else {
         testButton2();
@@ -172,7 +173,7 @@ function testButton2() {
     case 4:
       if (
         //rndWord.length <= row && rndWord.length >= col &&
-        row + rndWord.length <= riddleArray.length  &&
+        row + rndWord.length <= riddleArray.length &&
         col - rndWord.length >= 0 &&
         testWord(cell, row, col, switchRandomizer) == true
       ) {
@@ -180,7 +181,7 @@ function testButton2() {
           const cell = document.getElementById("" + row++ + col--);
           cell.innerHTML = "";
           cell.innerHTML += rndWord[0 + k];
-          numberCompareArray.push(cell.id)
+          numberCompareArray.push(cell.id);
         }
       } else {
         testButton2();
@@ -188,20 +189,19 @@ function testButton2() {
       break;
     // ------ R O W S - R E V E R S E ---------
     case 5:
-      
       if (
-        (row + rndWord.length <= riddleArray.length) &&
-        (testWord(cell, row, col, switchRandomizer) == true)
+        row + rndWord.length <= riddleArray.length &&
+        testWord(cell, row, col, switchRandomizer) == true
       ) {
         //rndWord = reverseString(rndWord);
         for (let k = 0; k < rndWord.length; k++) {
           const cell = document.getElementById("" + row++ + col);
           cell.innerHTML = "";
           cell.innerHTML += rndWord[0 + k];
-          numberCompareArray.push(cell.id)
+          numberCompareArray.push(cell.id);
         }
       } else {
-       testButton2();
+        testButton2();
       }
       break;
     // ----- C O L U M N S - R E V E R S E------
@@ -210,12 +210,12 @@ function testButton2() {
         col + rndWord.length <= riddleArray.length &&
         testWord(cell, row, col, switchRandomizer) == true
       ) {
-      //  rndWord = reverseString(rndWord);
+        //  rndWord = reverseString(rndWord);
         for (let k = 0; k < rndWord.length; k++) {
           const cell = document.getElementById("" + row + col++);
           cell.innerHTML = "";
           cell.innerHTML += rndWord[0 + k];
-          numberCompareArray.push(cell.id)
+          numberCompareArray.push(cell.id);
         }
       } else {
         testButton2();
@@ -226,14 +226,14 @@ function testButton2() {
       if (
         row + rndWord.length <= riddleArray.length &&
         col + rndWord.length <= riddleArray.length &&
-       testWord(cell, row, col, switchRandomizer) == true
+        testWord(cell, row, col, switchRandomizer) == true
       ) {
-      //  rndWord = reverseString(rndWord);
+        //  rndWord = reverseString(rndWord);
         for (let k = 0; k < rndWord.length; k++) {
           const cell = document.getElementById("" + row++ + col++);
           cell.innerHTML = "";
           cell.innerHTML += rndWord[0 + k];
-          numberCompareArray.push(cell.id)
+          numberCompareArray.push(cell.id);
         }
       } else {
         testButton2();
@@ -242,32 +242,31 @@ function testButton2() {
     // ----- D I A G O N A L - R E V E R S E TopRight to BottomLeft------
     case 8:
       if (
-        rndWord.length <= row && rndWord.length >= col &&
+        rndWord.length <= row &&
+        rndWord.length >= col &&
         row + rndWord.length <= riddleArray.length &&
         col - rndWord.length >= 0 &&
-       testWord(cell, row, col, switchRandomizer) == true
+        testWord(cell, row, col, switchRandomizer) == true
       ) {
-     //   rndWord = reverseString(rndWord);
+        //   rndWord = reverseString(rndWord);
         for (let k = 0; k < rndWord.length; k++) {
           const cell = document.getElementById("" + row++ + col--);
           cell.innerHTML = "";
           cell.innerHTML += rndWord[0 + k];
-          numberCompareArray.push(cell.id)
-          
+          numberCompareArray.push(cell.id);
         }
       } else {
         testButton2();
-      } 
+      }
       break;
 
     default:
       break;
 
-    
     //return cell.innerHTML;
   }
 }
-let numberCompareArray = []
+let numberCompareArray = [];
 /**************************W O R D - M A T C H - C H E C K E R***************** */
 function testWord(bool, row2, col2, switchRandomizer2) {
   var cell = bool;
@@ -275,13 +274,16 @@ function testWord(bool, row2, col2, switchRandomizer2) {
   var col = col2;
   var switchRandomizer = switchRandomizer2;
 
- 
   switch (switchRandomizer2) {
-    case 1: case 5:
-      console.log("dir5")
+    case 1:
+    case 5:
+      console.log("dir5");
       for (let k = 0; k < rndWord.length; k++) {
         cell = "" + (row + k) + col;
-        if (document.getElementById(cell).textContent !== "0" && rndWord[0 + k]  !== document.getElementById(cell).textContent) {
+        if (
+          document.getElementById(cell).textContent !== "0" &&
+          rndWord[0 + k] !== document.getElementById(cell).textContent
+        ) {
           bool = false;
           // window.alert(rnd);
           return bool;
@@ -291,20 +293,24 @@ function testWord(bool, row2, col2, switchRandomizer2) {
       }
       return bool;
 
-    case 2: case 6:
+    case 2:
+    case 6:
       for (let k = 0; k < rndWord.length; k++) {
         cell = "" + row + (col + k);
-        if (document.getElementById(cell).textContent !== "0"  && rndWord[0 + k] !== document.getElementById(cell).textContent) {
+        if (
+          document.getElementById(cell).textContent !== "0" &&
+          rndWord[0 + k] !== document.getElementById(cell).textContent
+        ) {
           bool = false;
           return bool;
         } else {
           bool = true;
         }
-        
       }
       return bool;
 
-    case 3: case 7:
+    case 3:
+    case 7:
       for (let k = 0; k < rndWord.length; k++) {
         cell = "" + (row + k) + (col + k);
         if (
@@ -319,7 +325,8 @@ function testWord(bool, row2, col2, switchRandomizer2) {
       }
       return bool;
 
-    case 4: case 8:
+    case 4:
+    case 8:
       for (let k = 0; k < rndWord.length; k++) {
         cell = "" + (row + k) + (col - k);
         if (
@@ -344,11 +351,11 @@ function drawRiddle() {
 
   for (let x = wordArray.length - 1; x >= 0; x--) {
     rndWord = wordArray[x];
-    rndWord = rndWord.split('');
+    rndWord = rndWord.split("");
     compareArray.push(rndWord);
-   // console.log(compareArray);
+    // console.log(compareArray);
     testButton2();
-  } 
+  }
   replaceZero();
 }
 /*
@@ -361,22 +368,18 @@ function drawRiddle() {
 var output = getAllLetters('Radagast');
  */
 
-
-
 /*****************************R E P L A C E - Z E R O S ***********************************************/
 function replaceZero() {
   riddleArray.forEach((row, i) =>
     row.forEach((item, j) => {
       if (document.getElementById("" + i + j).innerHTML === "0") {
-      document.getElementById("" + i + j).innerHTML = 0 //newRandomLetter();
+        document.getElementById("" + i + j).innerHTML = 0; //newRandomLetter();
       }
     })
   );
 }
 
-
 /*************************Mouse Event - C L I C K - O B S E R V E R  ******************************* */
-
 
 //let testSet = new Set();
 let wordCheck = "";
@@ -384,67 +387,66 @@ let letterArray = [];
 var storedCell;
 //var storedCell = letterArray.slice(-1)
 
-
 $("td").on("click", function () {
   let newCell = this.id[0] + this.id[1];
- // testSet.add(this.id);
-  
-  
+  // testSet.add(this.id);
+
   //console.log(testSet.entries())
   if (letterArray <= 0) {
     storedCell = newCell;
-    this.classList.add('boxHighlight')
+    this.classList.add("boxHighlight");
     $(this).css("background-color", "#8a8a8a");
-    letterArray.push(this.innerHTML); 
+    letterArray.push(this.innerHTML);
   } else {
     // console.log(storedCell)
-    var dx = Math.abs (newCell[0] - storedCell[0]);
-    var dy = Math.abs (newCell[1] - storedCell[1]);
-      if ((dx <= 1) && (dy <= 1)) {          // direkte Nachbarzelle getroffen
-        this.classList.add('boxHighlight')
-        $(this).css("background-color", "#8a8a8a");
-        letterArray.push(this.innerHTML); 
-        wordCheck = letterArray.join("")
-        storedCell = newCell;
-        if (wordArray.includes(wordCheck)) {
-           letterArray.forEach(element => $('.boxHighlight').css("background-color", "green")); 
-           $("td").removeClass("boxHighlight") 
-        }
-
-       // console.log($(".boxHighlight"))
-      } else {
-        $(".boxHighlight").css("background-color", "");
-        $("td").removeClass("boxHighlight")
-        letterArray = [];
-        /* this.classList.add('boxHighlight')
+    var dx = Math.abs(newCell[0] - storedCell[0]);
+    var dy = Math.abs(newCell[1] - storedCell[1]);
+    if (dx <= 1 && dy <= 1) {
+      // direkte Nachbarzelle getroffen
+      this.classList.add("boxHighlight");
+      $(this).css("background-color", "#8a8a8a");
+      letterArray.push(this.innerHTML);
+      wordCheck = letterArray.join("");
+      storedCell = newCell;
+      if (wordArray.includes(wordCheck)) {
+        letterArray.forEach((element) =>
+          $(".boxHighlight").css("background-color", "green")
+        );
+        $("td").removeClass("boxHighlight");
+        $("#" + wordCheck).css({
+          "text-decoration": "line-through",
+          color: "#8a8a8a",
+        });
+      }
+    } else {
+      $(".boxHighlight").css("background-color", "");
+      $("td").removeClass("boxHighlight");
+      letterArray = [];
+      /* this.classList.add('boxHighlight')
         $(this).css("background-color", "#8a8a8a");
         letterArray.push(this.innerHTML); */
-        //testSet.clear();
-      }  
-      }
-})
- 
+      //testSet.clear();
+    }
+  }
+});
+
+// $("li#newWordEntry.listedItems").fi
 
 /*************** M A R K - W O R D - A S - C O M P L E T E ***********/
 
-
-
-
-
-
-
 /************************T E S T - A R R A Y ************************/
-
 
 /*+++++TO DO's+++
 - get cell coords on event -- done for lookup
 - add a wordbuildig array on screen MAYBE
-- grey out words if they are found 
+- add directional intelligence
 - mark words, in case they did not enter the riddle
-- have the color events react correctly (as in remove their css properties)
+
 - add a self-complete function?
 - have words enter counter-diagonal -- done
 - get cell coords on event -- done
+- grey out words if they are found -- done
+- have the color events react correctly (as in remove their css properties) -- done
 */
 /*******************O P T I O N S - F O R - B O X - C H E C K *************** */
 /*
@@ -465,6 +467,13 @@ function clicktest() => {
 */
 
 /*+++++++++++++++++++++++H E L P F U L - F U N C T I O N S +++++++++++++++++++*/
+
+/**** filters through items *******/
+/* 
+$(filterItems(wordArray, wordCheck))
+function filterItems(arr, query) {
+  return arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+} */
 
 /******** allows mousedown to react to entire length of mousedown, not just entry *** */
 
@@ -488,7 +497,6 @@ document.onmouseup = () => {
 /* class hiddenWords {
   constructor(rndWord, )
 } */
-
 
 /*
 document.addEventListener('mouseover', () => {
